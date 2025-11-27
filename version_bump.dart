16cfbae3dfa0fcd24acf2628bd63afb631d6e5f9
Niladri Das <bniladridas@users.noreply.github.com>
2025-11-28 04:24:14 +0530

    fix: add linting checks and yaml fixes

--- version_bump.dart
+++ version_bump.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
-void main() {
+void main(List<String> args) {
+  final bumpType = args.isNotEmpty ? args[0] : 'patch';
+
   final versionFile = File('VERSION');
   if (!versionFile.existsSync()) {
     print('VERSION file not found');
@@ -14,12 +16,28 @@ void main() {
     exit(1);
   }
 
-  final major = int.parse(parts[0]);
-  final minor = int.parse(parts[1]);
-  final patch = int.parse(parts[2]) + 1;
+  var major = int.parse(parts[0]);
+  var minor = int.parse(parts[1]);
+  var patch = int.parse(parts[2]);
+
+  switch (bumpType) {
+    case 'major':
+      major++;
+      minor = 0;
+      patch = 0;
+      break;
+    case 'minor':
+      minor++;
+      patch = 0;
+      break;
+    case 'patch':
+    default:
+      patch++;
+      break;
+  }
 
   final newVersion = '$major.$minor.$patch';
   versionFile.writeAsStringSync('$newVersion\n');
 
-  print('Version bumped from $currentVersion to $newVersion');
+  print('Version bumped from $currentVersion to $newVersion ($bumpType)');
 }
